6b0f465368826ccd66f1c5de7ab6d37961f4f4d4
lwt <liwentao1a@163.com>
2020-11-24 10:47:32 +0800

    123456

---
+++ PythonReptiles/demo.py
@@ -0,0 +1,10 @@
+import urllib.request  
+import requests 
+import chardet
+# response= urllib.request.urlopen('https://www.runoob.com/bootstrap/bootstrap-tutorial.html')
+# html = response.read()
+# print(len(html))
+r=requests.get('https://www.runoob.com/bootstrap/bootstrap-tutorial.html')
+print(chardet.detect(r.content))
+r.encoding=chardet.detect(r.content)['encoding']
+print(r.text)
---
+++ PythonReptiles/taskManager.py
@@ -0,0 +1,45 @@
+#coding:utf-8
+#taskManager.py for windows
+import queue
+from multiprocessing.managers import BaseManager 
+from multiprocessing import freeze_support
+#任务个数
+task_number=10
+#定义收发队列
+task_queue=queue.Queue(task_number)
+result_gueue=queue.Queue(task_number)
+def get_task():
+    return task_queue 
+def get_result():
+    return result_gueue
+    #创建类似的QueueManager：
+class QueueManager(BaseManager):
+    pass
+def win_run():
+    #Windows下绑定调用接口不能使用1ambda，所以只能先定义函数再绑定
+    QueueManager.register('get_task_queue',callable=get_task)
+    QueueManager.register('get_result_queue',callable=get_result)
+    #绑定端口并设置验证口令，Windows下需要填写IP地址，Linux下不填默认为本地
+    manager=QueueManager(address=('127.0.0.1',8002),authkey='qiye'.encode())
+    #启动
+    manager.start()
+    try:
+        #通过网络获取任务队列和结果队列
+        task=manager.get_task_queue()
+        result=manager.get_result_queue()
+        #添加任务
+        for url in ["ImageUrl_"+str(i)for i in range(10)]:
+            print('put task %s...'% url)
+            task.put(url)
+        print ('try get result...')
+        while(True):
+            print('result is %s'%result.get(timeout=10))
+    except Exception as e:
+        print('Manager error %s' % e)
+    finally:
+        #一定要关闭，否则会报管道未关闭的错误
+        manager.shutdown()
+
+if __name__=='__main__':
+    freeze_support()
+    win_run()
